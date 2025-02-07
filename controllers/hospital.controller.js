cb00f38fedaa95ee407076284db0caa60d87ce55
surajmendhe5573 <surajmendhe470@gmail.com>
2025-02-07 15:07:26 +0530

    update api added

--- controllers/hospital.controller.js
+++ controllers/hospital.controller.js
@@ -27,4 +27,50 @@ const addHospital= async(req, res)=>{
         }
 };
 
-module.exports= addHospital;
+const updateHospital= async(req, res)=>{
+    try {
+        const {id}= req.params;
+        const {name, address, contactNumber, departments}= req.body;
+
+        if(req.user.role !== 'Admin'){
+            return res.status(403).json({message: 'Access denied. Only admins can add hospitals.'});
+        }
+
+        const updates= {};
+        if(name) updates.name= name;
+        if(address) updates.address= address;
+        if(contactNumber) updates.contactNumber= contactNumber;
+        if(departments) updates.contactNumber= contactNumber;
+
+        const updateHospital= await Hospital.findByIdAndUpdate(id, updates, {new:true});
+        if(!updateHospital){
+            return res.status(404).json({message: 'Hospital not found'});
+        }
+
+        res.status(200).json({message: 'Hospital updated succssfully', hospital:updateHospital});
+    } catch (error) {
+      res.status(500).json({message: 'Internal server error'});  
+    }
+};
+
+const deleteHospital= async(req, res)=>{
+    try {
+        const {id}= req.params;
+
+        if(req.user.role !== 'Admin'){
+            return res.status(403).json({message: 'Access denied. Only admins can add hospitals.'});
+        }
+
+        const deleteHospital= await Hospital.findByIdAndDelete(id);
+        if(!deleteHospital){
+            return res.status(404).json({message: 'Hospital not found'});
+        }
+
+        res.status(200).json({message: 'Hospital deleted successsfully'});
+        
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+module.exports= {addHospital, updateHospital, deleteHospital};
--- routes/hospital.js
+++ routes/hospital.js
@@ -1,8 +1,10 @@
 const express= require('express');
-const addHospital = require('../controllers/hospital.controller');
+const {addHospital, updateHospital, deleteHospital} = require('../controllers/hospital.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 const router= express.Router();
 
 router.post('/', authenticate, addHospital);
+router.put('/:id', authenticate, updateHospital);
+router.delete('/:id', authenticate, deleteHospital);
 
 module.exports= router;
