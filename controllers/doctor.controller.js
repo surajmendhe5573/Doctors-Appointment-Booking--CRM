4e68d5e9759e389c188c8b479ed09ae4525ab46d
surajmendhe5573 <surajmendhe470@gmail.com>
2025-02-07 16:25:55 +0530

    done

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -33,4 +33,52 @@ const addDoctor = async (req, res) => {
   }
 };
 
-module.exports = { addDoctor };
+const updateDoctor = async (req, res) => {
+    try {
+      const { user, specialities, qualifications, availability } = req.body;
+      const { doctorId } = req.params; 
+  
+      if (req.user.role !== 'Admin') {
+        if (req.user.id !== user) {
+          return res.status(403).json({ message: 'Access denied. You can only update your own profile.' });
+        }
+      }
+  
+      const doctor = await Doctor.findById(doctorId);
+      if (!doctor) {
+        return res.status(404).json({ message: 'Doctor not found' });
+      }
+  
+      // Check if the associated user is a doctor
+      const doctorUser = await User.findById(user);
+      if (!doctorUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      if (doctorUser.role !== 'Doctor') {
+        return res.status(400).json({ message: 'The associated user must be a doctor.' });
+      }
+  
+      doctor.specialities = specialities || doctor.specialities;
+      doctor.qualifications = qualifications || doctor.qualifications;
+      doctor.availability = availability || doctor.availability;
+  
+      if (user) {
+        const updatedUser = await User.findByIdAndUpdate(user, { $set: req.body.userDetails }, { new: true });
+        if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+      }
+  
+      await doctor.save();
+      res.status(200).json({
+        message: 'Doctor information updated successfully',
+        doctor
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+
+  
+module.exports = { addDoctor, updateDoctor };
--- routes/doctor.js
+++ routes/doctor.js
@@ -1,8 +1,9 @@
 const express= require('express');
-const { addDoctor } = require('../controllers/doctor.controller');
+const { addDoctor, updateDoctor } = require('../controllers/doctor.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 const router= express.Router();
 
 router.post('/', authenticate, addDoctor);
+router.put('/:doctorId', authenticate, updateDoctor);
 
 module.exports= router;
